99fdde55539a28572e433533e4ad571de0f71a36
carlesgenero-arch <carlesgenero@gmail.com>
2026-02-25 14:04:30 +0100

    Practica finalizada

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,15 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { INew } from '../../interfaces/inew.interface';
 import { NOTICIAS } from '../../db/noticias.db';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [DatePipe,FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
 
   misNoticias = signal<INew[]>(NOTICIAS)
+  nuevaNoticia: INew = {titulo:"", texto:"", imagen: "", fecha:""}
+
+  getDataForm(){
+    if(this.nuevaNoticia.titulo != "" && this.nuevaNoticia.texto !="" && this.nuevaNoticia.fecha !="" && this.nuevaNoticia.imagen !=""){
+      this.misNoticias.update((noticias)=>[...noticias,this.nuevaNoticia])
+      this.nuevaNoticia = {titulo:"", texto:"", imagen: "", fecha:""}
+
+    }else{
+      alert ('Los campos no pueden estar vacios')
+    }
+
+  }
 
 }
