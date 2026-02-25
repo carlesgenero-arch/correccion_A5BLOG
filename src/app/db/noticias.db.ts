f20f778ab75743b6d980d3c181134c44c927057e
carlesgenero-arch <carlesgenero@gmail.com>
2026-02-25 13:32:05 +0100

    Criterio 4 finished

---
+++ src/app/db/noticias.db.ts
@@ -0,0 +1,16 @@
+import { INew } from '../interfaces/inew.interface';
+
+export const NOTICIAS: INew[] = [
+  {
+    titulo: 'Angular 19 lanzado con nuevas características',
+    imagen: 'https://www.google.com/imgres?q=imagen%20angular%20300x200&imgurl=https%3A%2F%2Fwww.dzoom.org.es%2Fwp-content%2Fuploads%2F2022%2F04%2Flentes-ojo-de-pez-fisheye-retrato-paisaje-urbano-portada-300x200.jpg&imgrefurl=https%3A%2F%2Fwww.dzoom.org.es%2Ftag%2Fangular%2F&docid=Yaf7KWI96mgT2M&tbnid=j84SKPM3hmnpuM&vet=12ahUKEwjTouSO0vSSAxW5VKQEHSe6BVwQnPAOegQIIRAB..i&w=300&h=200&hcb=2&itg=1&ved=2ahUKEwjTouSO0vSSAxW5VKQEHSe6BVwQnPAOegQIIRAB',
+    texto: 'Se ha lanzado la última versión de Angular con mejoras significativas en el rendimiento y nuevas características de señales.',
+    fecha: '2024-12-20'
+  },
+  {
+    titulo: 'TypeScript 5.3 mejora la experiencia del desarrollador',
+    imagen: 'https://www.google.com/imgres?q=typescript%20300x200&imgurl=https%3A%2F%2Fwww.itdo.com%2Fblog%2Fcontent%2Fimages%2F2021%2F07%2Fitdo-que-es-type-script.jpg&imgrefurl=https%3A%2F%2Fwww.itdo.com%2Fblog%2Fque-es-typescript-y-por-que-utilizarlo%2F&docid=AW6UqYdkdItnKM&tbnid=aVsM9-sXhsgJcM&vet=12ahUKEwjb8ICe0vSSAxV-KvsDHZc_Dp8QnPAOegQILxAB..i&w=999&h=666&hcb=2&ved=2ahUKEwjb8ICe0vSSAxV-KvsDHZc_Dp8QnPAOegQILxAB',
+    texto: 'La nueva versión de TypeScript incluye optimizaciones de tipos y mejor soporte para características modernas de JavaScript.',
+    fecha: '2024-12-15'
+  }
+];
